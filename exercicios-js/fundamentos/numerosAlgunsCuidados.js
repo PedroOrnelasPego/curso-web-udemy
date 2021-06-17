500abc4677ebad1784dc71a4d7cd3c70c8d4c710
Pedro Ornelas <pedroornelaspego@gmail.com>
2021-06-16 22:22:10 -0300

    Create numerosAlgunsCuidados.js

---
+++ exercicios-js/fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,5 @@
+console.log(7 / 0) // Infinity
+console.log("10" / 2) //igual a 5!
+console.log("Show!" * 2) //NaN not a number
+console.log(0.1 + 0.7) //0.7999999999999999
+console.log((10).toFixed(2))
