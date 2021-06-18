4552ee95783c5d5f5de8e94615d507a670e6034a
Pedro Ornelas <pedroornelaspego@gmail.com>
2021-06-18 19:19:18 -0300

    Create arrays.js

---
+++ exercicios-js/fundamentos/arrays.js
@@ -0,0 +1,19 @@
+const valores = [7.7, 8.9, 6.3, 9.2]
+console.log(valores[0], valores[3]) //7.7 e 9.2
+console.log(valores[4]) //undefined
+
+valores[10] = 10
+console.log(valores) //[ 7.7, 8.9, 6.3, 9.2, <6 empty items>, 10 ]
+
+valores[4] = 10
+console.log(valores)
+console.log(valores.length) //mostrar quantos elementos
+
+valores.push({id: 3}, false, null, 'teste')
+console.log(valores)
+
+console.log(valores.pop())
+delete valores[0]
+console.log(valores)
+
+console.log(typeof valores)
