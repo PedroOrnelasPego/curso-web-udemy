f6c74246c0e4056c6f357c6ba9257a4a4173a637
Pedro Ornelas <pedroornelaspego@gmail.com>
2021-06-21 14:11:50 -0300

    Create objeto.js

---
+++ exercicios-js/fundamentos/objeto.js
@@ -0,0 +1,20 @@
+const prod1 = {} //utilizar {} para criar objetos.
+prod1.name = 'Celular Ultra Mega'
+prod1.price = 4998.90
+prod1['desconto legal'] = 0.40 //evitar atribudos com espaço.
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Pedro',
+    preco: 79.90.toFixed(2) //duas casas decimais.
+}
+
+console.log(prod2)
+
+
+
+/*Json*/
+'{ "nome": "Pedro", "preco": 79.90}'
+
+
