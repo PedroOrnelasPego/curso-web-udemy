61c0725dbdee5093af3d4781006a874de4539441
Pedro Ornelas <pedroornelaspego@gmail.com>
2021-07-09 14:56:42 -0300

    Create 04-divisão.js

---
+++ exercicios-js/04-divisão.js
@@ -0,0 +1,12 @@
+function divisão(a, b) {
+    console.log(`O Resultado da divisão é ${Math.floor (a / b)} e o resto da divisão é ${a % b}.`)
+}
+
+divisão(10,2)
+divisão(6,2)
+divisão(8,6)
+divisão(100,8)
+divisão(200,4)
+divisão(11,4)
+
+// Math.floor retorna o menor numero inteiro.
