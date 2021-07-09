baabefb81a9214d560dda49686e1e74899b24e73
Pedro Ornelas <pedroornelaspego@gmail.com>
2021-07-09 13:55:16 -0300

    Create 03-expoente.js

---
+++ exercicios-js/03-expoente.js
@@ -0,0 +1,10 @@
+function expoente(base,expoente) {
+    return Math.pow(base,expoente)
+    
+    //  //Método novo:
+    //  resultado = base ** expoente
+
+    //  return resultado
+}
+
+console.log(expoente(2, 5))
